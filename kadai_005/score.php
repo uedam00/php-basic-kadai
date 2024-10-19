59781535a0012a064d3e96a182d20ab3dfef3842
澤田雄一 <60324126+uedam00@users.noreply.github.com>
2024-10-19 11:35:56 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,19 @@
+<?php
+$score1 = 80;
+$score2 = 60;
+$score3 = 55;
+$score4 = 40;
+$score5 = 100;
+$score6 = 25;
+$score7 = 80;
+$score8 = 95;
+$score9 = 30;
+$score10 = 60;
+
+$score = $score1 + $score2 + $score3 + $score4 +$score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+echo '合計点'.$score;
+echo '<br>';
+
+$average = $score / 10;
+echo '平均点'.$average;
+?>
